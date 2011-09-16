cc8af222b09ebc9a7fdc7ac5f5808dfb3615171f
ethan@5k6.org <ethan@5k6.org@a9ab38be-9f86-eefd-77fc-32d8e7642564>
2011-09-16 09:06:20 +0000

    framework-2.0.0

---
+++ tags/framework-2.0.0/src/main/java/org/chinasb/framework/modules/demo/dao/DemoDao.java
@@ -0,0 +1,11 @@
+package org.chinasb.framework.modules.demo.dao;
+
+import org.chinasb.framework.core.base.dao.hibernate.GenericDAO;
+import org.chinasb.framework.modules.demo.model.Demo;
+
+/**
+ * @author ethan
+ */
+public interface DemoDao extends GenericDAO<Demo, Integer> {
+
+}
---
+++ tags/framework-2.0.0/src/test/java/org/chinasb/framework/modules/demo/action/DemoActionTest.java
@@ -0,0 +1,65 @@
+package org.chinasb.framework.modules.demo.action;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.chinasb.framework.core.base.test.BaseTestTemplate;
+import org.chinasb.framework.modules.demo.model.Demo;
+import org.chinasb.framework.modules.demo.service.DemoService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+
+/**
+ * @author ethan
+ */
+public class DemoActionTest extends BaseTestTemplate {
+
+	@Resource
+	private DemoService demoService;
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("测试开始");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("测试结束");
+	}
+
+	@BeforeTransaction
+	public void beforeTransaction() {
+		System.out.println("事务开始");
+	}
+
+	@AfterTransaction
+	public void afterTransaction() {
+		System.out.println("事务结束");
+	}
+
+	@Test
+	public void testInsert() {
+		Demo demo = new Demo();
+		demo.setTitle("junit-test");
+		demo.setContent("junit-content");
+		demo.setPublishdate(new Date());
+		demoService.save(demo);
+	}
+
+	@Test
+	public void testInsertMore() {
+		Demo[] demos = new Demo[10];
+		for (int i = 0; i < 10; i++) {
+			Demo demo = new Demo();
+			demo.setTitle("junit-test" + i);
+			demo.setContent("junit-content" + i);
+			demo.setPublishdate(new Date());
+			demos[i] = demo;
+		}
+		demoService.save(demos);
+	}
+}
