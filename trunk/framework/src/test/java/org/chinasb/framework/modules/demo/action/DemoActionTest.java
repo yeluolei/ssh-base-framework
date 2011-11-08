3afa27497aa78d69d21fe15be34f4e8e3da9ca1a
ethan@5k6.org <ethan@5k6.org@a9ab38be-9f86-eefd-77fc-32d8e7642564>
2011-11-08 18:57:38 +0000

    增加Search接口测试实例

--- trunk/framework/src/test/java/org/chinasb/framework/modules/demo/action/DemoActionTest.java
+++ trunk/framework/src/test/java/org/chinasb/framework/modules/demo/action/DemoActionTest.java
@@ -1,6 +1,7 @@
 package org.chinasb.framework.modules.demo.action;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -12,6 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.context.transaction.AfterTransaction;
 import org.springframework.test.context.transaction.BeforeTransaction;
+
+import com.googlecode.genericdao.search.Search;
+import com.googlecode.genericdao.search.SearchResult;
 
 /**
  * @author ethan
@@ -44,7 +48,7 @@ public class DemoActionTest extends BaseTestTemplate {
 	@Test
 	public void testInsert() {
 		Demo demo = new Demo();
-		demo.setTitle("junit-test");
+		demo.setTitle("junit-testing");
 		demo.setContent("junit-content");
 		demo.setPublishdate(new Date());
 		demoService.save(demo);
@@ -55,11 +59,78 @@ public class DemoActionTest extends BaseTestTemplate {
 		Demo[] demos = new Demo[10];
 		for (int i = 0; i < 10; i++) {
 			Demo demo = new Demo();
-			demo.setTitle("junit-test" + i);
+			demo.setTitle("junit-testing" + i);
 			demo.setContent("junit-content" + i);
 			demo.setPublishdate(new Date());
 			demos[i] = demo;
 		}
 		demoService.save(demos);
+	}
+
+	@Test
+	public void testFindAll() {
+		List<Demo> list = demoService.findAll();
+		for (Demo demo : list) {
+			System.out.println(demo.getTitle());
+		}
+	}
+
+	@Test
+	public void testSearch() {
+		Search search = new Search();
+		search.addFilterEqual("title", "junit-testing");
+		List<Demo> list = demoService.search(search);
+		for (Demo demo : list) {
+			System.out.println(demo.getTitle());
+		}
+	}
+
+	@Test
+	public void testSearchCount() {
+		Search search = new Search();
+		search.addFilterEqual("title", "junit-testing");
+		int count = demoService.count(search);
+		System.out.println(count);
+	}
+
+	@Test
+	public void testSearchAndCount() {
+		Search search = new Search();
+		search.addFilterEqual("title", "junit-testing");
+		SearchResult<Demo> result = demoService.searchAndCount(search);
+		List<Demo> list = result.getResult();
+		for (Demo demo : list) {
+			System.out.println(demo.getTitle());
+		}
+		int count = result.getTotalCount();
+		System.out.println(count);
+	}
+
+	@Test
+	public void testSearchInterface() {
+		Search search = new Search();
+		// 每页记录数
+		search.setMaxResults(17);
+		// 当前页码
+		search.setPage(3);
+
+		// 根据条件查询当前页码记录集
+		List<Demo> list = demoService.search(search);
+		for (Demo demo : list) {
+			System.out.println(demo.getTitle());
+		}
+
+		// 根据条件查询记录集总数
+		int totalResults = demoService.count(search);
+		System.out.println(totalResults);
+
+		// 根据条件查询记录集及总记录数
+		SearchResult<Demo> result = demoService.searchAndCount(search);
+		list = result.getResult();
+		for (Demo demo : list) {
+			System.out.println(demo.getTitle());
+		}
+		totalResults = result.getTotalCount();
+		System.out.println(totalResults);
 	}
 }
